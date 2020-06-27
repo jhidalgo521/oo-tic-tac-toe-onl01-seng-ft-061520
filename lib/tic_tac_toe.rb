286f594c1e230c674d08dba498b74fe341a13230
Jose Hidago <jhidalgo521@gmail.com>
2020-06-27 20:41:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,12 @@ class TicTacToe
   end 
   
   def display_board
+    
     puts "#{@board[0]} |#{@board[2]} | #{@board[3]}"
+    
+    puts "#{@board[4]} |#{@board[5]} | #{@board[6]}"
+    
+    puts "#{@board[7]} |#{@board[8]} | #{@board[9]}"
     
   end 
   
