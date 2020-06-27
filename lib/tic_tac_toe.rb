f3ea8efa8f64cefcffcf2a8e731c7acee802800c
Jose Hidago <jhidalgo521@gmail.com>
2020-06-27 21:59:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -161,11 +161,18 @@ class TicTacToe
   
   
   def play
-  
-  
-  
-  
-  
+    
+    while over? == false
+      turn
+    end
+    
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+    
+  end
   
   
 end 
