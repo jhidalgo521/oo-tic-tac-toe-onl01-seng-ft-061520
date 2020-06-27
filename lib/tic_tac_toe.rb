6272425c77813b10f14a00c2927fa117fb2bc031
Jose Hidago <jhidalgo521@gmail.com>
2020-06-27 21:32:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,11 @@ class TicTacToe
     
     turn = 0 
     
-    @board.each { |i| if i == "X" || i == "O" turn += 1}
+    @board.each do |i| 
+      if i == "X" || i == "O" 
+        turn += 1
+      end 
+    end 
     
   end 
   
